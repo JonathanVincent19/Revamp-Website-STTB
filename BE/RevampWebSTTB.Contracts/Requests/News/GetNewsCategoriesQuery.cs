c2255ae1c867a95aecb37e8b832cccd32953d7da
SieeneJ <Jessika9905@gmail.com>
2026-03-17 20:31:14 +0700

    Fix: Separate Respond and DTO from Request file

--- BE/RevampWebSTTB.Contracts/Requests/News/GetNewsCategoriesQuery.cs
+++ BE/RevampWebSTTB.Contracts/Requests/News/GetNewsCategoriesQuery.cs
@@ -1,20 +1,9 @@
 using MediatR;
+using RevampWebSTTB.Contracts.Responses.News;
 
 namespace RevampWebSTTB.Contracts.Requests.News
 {
     public record GetNewsCategoriesQuery : IRequest<GetNewsCategoriesResponse>
-    {
-    }
-    
-    public record GetNewsCategoriesResponse
-    {
-        public bool Success { get; init; }
-        public List<NewsCategoryDto> Data { get; init; } = new();
-    }
-    
-    public record NewsCategoryDto
-    {
-        public int Id { get; init; }
-        public string Name { get; init; } = string.Empty;
-    }
+        {
+        }
 }
